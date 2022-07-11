5d67eb586cdbc38c1b006fa4a712d3aa6061693a
Bhagwat Kushwaha <bhagwat.khushwaha@waiin.com>
2022-07-11 18:05:37 +0530

    update

--- raaghu-mfe/utils/generate-mfe.js
+++ raaghu-mfe/utils/generate-mfe.js
@@ -5,19 +5,18 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const tsconfigPath = path.join(__dirname, '..', 'tsconfig.json');
 
-if (process.argv.length > 5) {
-    console.log('Invalid command');
-    process.exit(0);
+if (process.argv.length > 4) {
+  console.log('Invalid command');
+  process.exit(0);
 }
 
 if (isNaN(process.argv[process.argv.length - 1])) {
-    console.log('Invalid port');
-    process.exit(0);
+  console.log('Invalid port');
+  process.exit(0);
 }
 
-const mfeName = process.argv[process.argv.length - 3];
-const port = process.argv[process.argv.length - 2];
-const templateName = process.argv[process.argv.length - 1]
+const mfeName = process.argv[process.argv.length - 2];
+const port = process.argv[process.argv.length - 1];
 
 console.log('Creating application...')
 execSync(`ng g application ${mfeName} --routing=true --style=scss`, { cwd: process.cwd(), stdio: 'inherit' });
@@ -31,9 +30,9 @@ let newWebpackFile = fs.readFileSync(path.join(__dirname, 'templates', 'webpack-
 const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath).toString());
 const commonLibs = Object.keys(tsconfig.compilerOptions.paths);
 while (newWebpackFile.includes('{{ mfeName }}')) {
-    newWebpackFile = newWebpackFile.replace('{{ mfeName }}', mfeName);
+  newWebpackFile = newWebpackFile.replace('{{ mfeName }}', mfeName);
 }
-newWebpackFile = newWebpackFile.replace('{{ commonLibs }}', `[${commonLibs.toString().split(',').map(r => r.trim()).map(r =>`'${r}'`).join(',')}]`);
+newWebpackFile = newWebpackFile.replace('{{ commonLibs }}', `[${commonLibs.toString().split(',').map(r => r.trim()).map(r => `'${r}'`).join(',')}]`);
 fs.writeFileSync(existingWebpackFilePath, newWebpackFile)
 
 
@@ -41,10 +40,5 @@ fs.writeFileSync(existingWebpackFilePath, newWebpackFile)
 const appRoutingModuleFilePath = path.join(__dirname, '..', 'projects', `${mfeName}`, 'src', 'app', 'app-routing.module.ts');
 const appRoutingModuleFileContent = fs.readFileSync(path.join(__dirname, 'templates', 'app-routing-template.module.ts')).toString();
 fs.writeFileSync(appRoutingModuleFilePath, appRoutingModuleFileContent)
-
-const navigationTemplatePath = path.join(__dirname, '..', 'projects', `${mfeName}`, 'src', 'app', 'app.component.html');
-const navigationTemplateFileContent = fs.readFileSync(path.join(__dirname, 'templates', `${templateName}`+'.html' )).toString();
-fs.writeFileSync(navigationTemplatePath, navigationTemplateFileContent)
-
 
 console.log('Done !!')
