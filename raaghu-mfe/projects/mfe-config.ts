fef01b17e9ec6a2593b23fbe724b732938ffac42
Bhagwat Kushwaha <bhagwat.khushwaha@waiin.com>
2022-07-11 15:50:11 +0530

    update url

--- raaghu-mfe/projects/mfe-config.ts
+++ raaghu-mfe/projects/mfe-config.ts
@@ -1,95 +1,95 @@
 export const MfeConfig = {
   host: {
-    url: 'https://demo.raaghu.io/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/remoteEntry.js',
   },
   product: {
-    url: 'https://demo.raaghu.io/product/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/product/remoteEntry.js',
   },
   dashboard: {
-    url: 'https://demo.raaghu.io/dashboard/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/dashboard/remoteEntry.js',
   },
   rdsComponents: {
-    url: 'https://demo.raaghu.io/rds-components/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/rds-components/remoteEntry.js',
   },
   tenant: {
-    url: 'https://demo.raaghu.io/tenant/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/tenant/remoteEntry.js',
   },
   demouicomponents: {
-    url: 'https://demo.raaghu.io/demo-ui/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/demo-ui/remoteEntry.js',
   },
   edition: {
-    url: 'https://demo.raaghu.io/edition/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/edition/remoteEntry.js',
   },
   maintenance: {
-    url: 'https://demo.raaghu.io/maintenance/remoteEntry.js',
+    url: 'https://stageui.raaghu.io/maintenance/remoteEntry.js',
   },
   language: {
-    url: 'https://demo.raaghu.io/language/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/language/remoteEntry.js',
   },
   auditlogs: {
-    url: 'https://demo.raaghu.io/audit-logs/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/audit-logs/remoteEntry.js',
   },
   role: {
-    url: 'https://demo.raaghu.io/role/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/role/remoteEntry.js',
   },
   visualsettings: {
-    url: 'https://demo.raaghu.io/visual-settings/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/visual-settings/remoteEntry.js',
   },
   subscription: {
-    url: 'https://demo.raaghu.io/subscription/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/subscription/remoteEntry.js',
   },
   forgotpassword: {
-    url: 'https://demo.raaghu.io/forgot-password/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/forgot-password/remoteEntry.js',
   },
   user: {
-    url: 'https://demo.raaghu.io/user/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/user/remoteEntry.js',
   },
   login: {
-    url: 'https://demo.raaghu.io/login/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/login/remoteEntry.js',
   },
   loginprofile: {
-    url: 'https://demo.raaghu.io/loginprofile/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/loginprofile/remoteEntry.js',
   },
   organizationunit: {
-    url: 'https://demo.raaghu.io/organization-unit/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/organization-unit/remoteEntry.js',
   },
   dynamicpropertymanagement: {
-    url: 'https://demo.raaghu.io/dynamic-property-management/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/dynamic-property-management/remoteEntry.js',
   },
   webhooksubscription: {
-    url: 'https://demo.raaghu.io/webhooksubscription/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/webhooksubscription/remoteEntry.js',
   },
   Claimtype: {
-    url: 'https://demo.raaghu.io/Claimtype/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/Claimtype/remoteEntry.js',
   },
   Securitylog: {
-    url: 'https://demo.raaghu.io/Securitylog/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/Securitylog/remoteEntry.js',
   },
   settings: {
-    url: 'https://demo.raaghu.io/settings/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/settings/remoteEntry.js',
   },
   client: {
-    url: 'https://demo.raaghu.io/client/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/client/remoteEntry.js',
   },
   identiyresources: {
-    url: 'https://demo.raaghu.io/identity-resources/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/identity-resources/remoteEntry.js',
   },
   texttemplate: {
-    url: 'https://demo.raaghu.io/text-template/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/text-template/remoteEntry.js',
   },
   apiresources: {
-    url: 'https://demo.raaghu.io/api-resources/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/api-resources/remoteEntry.js',
   },
   apiscope: {
-    url: 'https://demo.raaghu.io/api-scope/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/api-scope/remoteEntry.js',
   },
   memberactivity: {
-    url: 'https://demo.raaghu.io/member-activity/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/member-activity/remoteEntry.js',
   },
   monthlysummary: {
-    url: 'https://demo.raaghu.io/monthly-summary/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/monthly-summary/remoteEntry.js',
   },
   languagetext: {
-    url: 'https://demo.raaghu.io/language-text/remoteEntry.js',
+    url: '​https://stageui.raaghu.io/language-text/remoteEntry.js',
   },
 };
