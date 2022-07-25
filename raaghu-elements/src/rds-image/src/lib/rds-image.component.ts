ee963b807f1ffc2eaf39201f52c5e144e40340f4
Chinmay Wani <chinmay.wani@waiin.com>
2022-07-25 15:47:54 +0530

    creation of image element

--- raaghu-elements/src/rds-image/src/lib/rds-image.component.ts
+++ raaghu-elements/src/rds-image/src/lib/rds-image.component.ts
@@ -8,12 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RdsImageComponent implements OnInit {
 
   title = 'rds-image';
-  @Input() display_type: 'Basic'| 'Product_overview1' | 'Product_overview2' | 'Product_overview3' = 'Basic';
+  @Input() display_type?: string = 'Basic';
   
   
   constructor() { }
   @Input()
-  questionList= [    
+  itemList= [    
   "https://www.waiin.com/wp-content/uploads/2021/07/Framework-Expertise_01.png",
   "https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   "https://cdn.shopify.com/s/files/1/0752/6435/products/7_560d64a9-7d29-4ea4-93ed-7b6d29bd6339_1_765x.jpg?v=1639994439",
