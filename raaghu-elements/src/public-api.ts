e03876cbe90e2b222dd9b41191f47ec4ffd36903
Alok Nakhate <99717383+alok-nakhate@users.noreply.github.com>
2022-07-28 14:20:02 +0530

    Update public-api.ts

--- raaghu-elements/src/public-api.ts
+++ raaghu-elements/src/public-api.ts
@@ -182,10 +182,8 @@ export * from './rds-checkbox-groups/src/public-api';
 // Public API Surface of rds-fab-menu
 export * from './rds-fab-menu/src/public-api';
 
+// Public API Surface of rds-label
+export * from './rds-label/src/public-api';
+
 // Public API Surface of rds-fab-menu
 export * from './rds-rating/src/public-api';
-
-
-
-
-
