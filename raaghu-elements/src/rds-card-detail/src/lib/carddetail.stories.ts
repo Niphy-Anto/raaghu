27701e0322693f91f294af0d294d59a330a820d8
Anupriya-Biju <anupriya.biju@waiin.com>
2022-07-29 16:40:40 +0530

    added rds-card-detail

---
+++ raaghu-elements/src/rds-card-detail/src/lib/carddetail.stories.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { RdsIconModule } from '@libs/rds-icon';
+import { RdsRadioButtonModule } from '@libs/rds-radio-button';
+import { Story, Meta,moduleMetadata } from '@storybook/angular';
+import { RdsCardDetailComponent } from './rds-card-detail.component';
+
+export default {
+  title: 'Elements/Card Detail',
+  component: RdsCardDetailComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [],
+      imports: [CommonModule, RdsIconModule,RdsRadioButtonModule],
+    }),
+  ],
+  argTypes: {    
+    
+  },
+} as Meta;
+
+const Template: Story<RdsCardDetailComponent> = (args: RdsCardDetailComponent) => ({
+  props: args,
+  template: `<rds-card-detail [cardData]=cardData>
+   </rds-card-detail>`
+});
+
+// const teletext = "some text"
+export const CardDetail = Template.bind({});
+CardDetail.args = {
+    cardData:{cardID:'1011',cardName:'MasterCard',cardExpiry:'11/2027',cardLogo:'dashboard',cardNumber:1111,isDefault:false}
+};
+
