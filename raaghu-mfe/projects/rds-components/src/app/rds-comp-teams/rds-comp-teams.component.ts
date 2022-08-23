710952c8aa55c82d2b18208501c0ef748ba19709
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2022-08-23 18:26:30 +0530

    Adding Icon in teams elements

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-teams/rds-comp-teams.component.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-teams/rds-comp-teams.component.ts
@@ -18,32 +18,32 @@ export class RdsCompTeamsComponent implements OnInit {
       title: 'Tina',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     },
     {
       title: 'Wily',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     } ,
     {
       title: 'Vivek',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     } ,
     {
       title: 'Riya',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     }     
   ]
--- raaghu-mfe/projects/rds-components/src/app/rds-comp-teams/teams.stories.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-teams/teams.stories.ts
@@ -32,32 +32,32 @@ basic.args = {
           title: 'Tina',
           subTitle: 'Web Developer',
           imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-          twitterIcon: 'star',
-          linkdineIcon: 'star',
+          twitterIcon: 'twitter',
+          linkdineIcon: 'linkedin',
           description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
         },
         {
           title: 'Wily',
           subTitle: 'Web Developer',
           imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-          twitterIcon: 'star',
-          linkdineIcon: 'star',
+          twitterIcon: 'twitter',
+          linkdineIcon: 'linkedin',
           description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
         } ,
         {
           title: 'Vivek',
           subTitle: 'Web Developer',
           imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-          twitterIcon: 'star',
-          linkdineIcon: 'star',
+          twitterIcon: 'twitter',
+          linkdineIcon: 'linkedin',
           description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
         } ,
         {
           title: 'Riya',
           subTitle: 'Web Developer',
           imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-          twitterIcon: 'star',
-          linkdineIcon: 'star',
+          twitterIcon: 'twitter',
+          linkdineIcon: 'linkedin',
           description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
         }     
       ]
@@ -71,32 +71,32 @@ basic.args = {
       title: 'Tina',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     },
     {
       title: 'Wily',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     } ,
     {
       title: 'Vivek',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     } ,
     {
       title: 'Riya',
       subTitle: 'Web Developer',
       imgLink:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      twitterIcon: 'star',
-      linkdineIcon: 'star',
+      twitterIcon: 'twitter',
+      linkdineIcon: 'linkedin',
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
     }     
   ]
