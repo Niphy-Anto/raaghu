3a9c5ea2a6aa62a09f65b2dcb19aaa475f357619
Ketan-Khalate <ketan.khalate@waiin.com>
2022-09-20 13:11:02 +0530

    committted changes for open position component

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-open-position/rds-comp-open-position.component.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-open-position/rds-comp-open-position.component.ts
@@ -6,7 +6,56 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./rds-comp-open-position.component.scss']
 })
 export class RdsCompOpenPositionComponent implements OnInit {
-  @Input() item: any;  
+  @Input() item: any = {
+    titleHeader: "Open Positions",
+    navtabsItems: [
+      {
+        id: 'developer',
+        label: 'Developer',
+        tablink: '#developer',
+        ariacontrols: 'developer',
+      },
+      {
+        id: 'marketing',
+        label: 'Marketing',
+        tablink: '#marketing',
+        ariacontrols: 'marketing',
+      },
+      {
+        id: 'designer',
+        label: 'Designer',
+        tablink: '#designer',
+        ariacontrols: 'designer',
+      },
+      {
+        id: 'researcher',
+        label: 'Researcher',
+        tablink: '#researcher',
+        ariacontrols: 'researcher',
+      }
+    ],
+    descriptions: [
+      {
+        type: "marketing",
+        roleName: "Android Developer",
+        roleDescription: "Designing and developing advanced applications for the Android platform · Unit-testing code for robustness, ...",
+        buttonLabel: "Apply Now"
+      },
+      {
+        type: "designer",
+        roleName: "Android Developer",
+        roleDescription: "Designing and developing advanced applications for the Android platform · Unit-testing code for robustness, ...",
+        buttonLabel: "Apply Now"
+      },
+      {
+        type: "researcher",
+        roleName: "Android Developer",
+        roleDescription: "Designing and developing advanced applications for the Android platform · Unit-testing code for robustness, ...",
+        buttonLabel: "Apply Now"
+      },
+    ],
+  }
+;  
   activePage: number = 0;
   constructor() { }
 
