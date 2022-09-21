817012c6b3ca67fa897acf9cbea60388ea02e3b4
Vaishnavi Pardeshi <vaishnavi.pardeshi@waiin.com>
2022-09-21 16:37:32 +0530

    Enhancement in cookies for popup

--- raaghu-mfe/projects/host/src/app/app.module.ts
+++ raaghu-mfe/projects/host/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { AppComponent} from './app.component';
 import { HomeComponent } from './home/home.component';
 import { APP_ROUTES } from './app.routes';
 import { NotFoundComponent } from './not-found.component';
@@ -11,19 +11,48 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { MultipleMfeComponent } from './multiple-mfe/multiple-mfe.component';
 import { EffectsModule } from '@ngrx/effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule, UserAuthModule, NgxTranslateModule, API_BASE_URL } from '@libs/shared';
+import {
+  SharedModule,
+  UserAuthModule,
+  NgxTranslateModule,
+  API_BASE_URL,
+} from '@libs/shared';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { DatePipe } from '@angular/common';
 import {
-  LanguageEffects, ProductEffects, productReducer, DynamicEntityEffects, DynamicEntityReducer,
-  DynamicPropertyEffects, DynamicPropertyReducer, ProfileEffects, ProfileReducer, WebhookSubscriptionEffects,
-  LoginEffects, ValidateTenantReducer,
-  OrganizationUnitReducer, OrganizationUnitEffects, MLAReducer, MLAEffects,
-  MaintenanceEffects, ManageLinkedAccountsEffects, LoginAttemptsEffects,
-  DelegationsReducer, DelegationsEffects, GetAllDynamicProperty, GetAllDynamicPropertyEntites,
-  GetInputnameReducer, getDynamicPropertyByEditReducer, DynamicPermissionReducer,
-  GetUsernameFilterReducer, LoginAttemptsReducer, LanguageReducer, CountryListReducer, DefaultLanguageReducer, downloadReducer, DownloadEffects
-
+  LanguageEffects,
+  ProductEffects,
+  productReducer,
+  DynamicEntityEffects,
+  DynamicEntityReducer,
+  DynamicPropertyEffects,
+  DynamicPropertyReducer,
+  ProfileEffects,
+  ProfileReducer,
+  WebhookSubscriptionEffects,
+  LoginEffects,
+  ValidateTenantReducer,
+  OrganizationUnitReducer,
+  OrganizationUnitEffects,
+  MLAReducer,
+  MLAEffects,
+  MaintenanceEffects,
+  ManageLinkedAccountsEffects,
+  LoginAttemptsEffects,
+  DelegationsReducer,
+  DelegationsEffects,
+  GetAllDynamicProperty,
+  GetAllDynamicPropertyEntites,
+  GetInputnameReducer,
+  getDynamicPropertyByEditReducer,
+  DynamicPermissionReducer,
+  GetUsernameFilterReducer,
+  LoginAttemptsReducer,
+  LanguageReducer,
+  CountryListReducer,
+  DefaultLanguageReducer,
+  downloadReducer,
+  DownloadEffects,
 } from '@libs/state-management';
 import { LanguageTextEffects } from 'projects/libs/state-management/src/lib/state/language-text/language-text.effects';
 import { RdsSideNavModule } from '@libs/rds-elements';
@@ -32,42 +61,39 @@ import demodata from '../assets/appconfig.json';
 import { RdsCookieConsentConfig } from 'projects/libs/rds-cookieconsent/src/lib/service/cookieconsent-config';
 import { RdsCookieConsentModule } from 'projects/libs/rds-cookieconsent/src/lib/rds-cookieconsent.module';
 export function getRemoteServiceBaseUrl(): any {
-  let URL = demodata.remoteServiceBaseUrl
+  let URL = demodata.remoteServiceBaseUrl;
   return URL;
 }
-const cookieConfig: RdsCookieConsentConfig = {
+ const cookieConfig: RdsCookieConsentConfig = {
   cookie: {
     domain: location.hostname,
     name: 'rds_cookie_status'
   },
-  position: "bottom",
-  theme: "classic",
+  position: 'bottom',
+  theme: 'classic',
   palette: {
     popup: {
-      background: "#e8ebf9ed",
-      text: "#000000",
+      background: '#e8ebf9ed',
+      text: '#000000',
     },
     button: {
-      background: "#012fffb5",
-      text: "#000000",
-      border: "transparent"
-    }
+      background: '#012fffb5',
+      text: '#000000',
+      border: 'transparent',
+    },
   },
-  type: "opt-in",
+  type:'opt-in',
   elements: {
     messagelink: `
-     <img class="pe-3" src="{{image}}" width=\"80px\" ></img>
-      <span id="cookieconsent:desc" class="cc-message" >{{message}}</span>
-      `
-    },
-  content: {
-    policy: "Cookie Policy",
-    image: "../assets/cookie.svg",
-    
-  }
+      <img class="pe-3" src="{{image}}" width=\"80px\" ></img>
+      <span id="cookieconsent:desc" class="cc-message">{{message}} </span>
+      `,  
+  }, 
+  content: {    
+    policy: 'Rds Cookies',
+    image: '../assets/cookie.svg',   
+  } 
 };
-
-
 @NgModule({
   imports: [
     BrowserModule,
@@ -97,23 +123,33 @@ const cookieConfig: RdsCookieConsentConfig = {
       countries: CountryListReducer,
       languageText: LanguageTextReducer,
       defaultLanguage: DefaultLanguageReducer,
-      downloadData: downloadReducer
+      downloadData: downloadReducer,
     }),
 
     StoreDevtoolsModule.instrument({
       name: 'Raaghu MFE',
       logOnly: false,
     }),
-    EffectsModule.forRoot([ProductEffects, LoginAttemptsEffects, MLAEffects,
-      LanguageEffects, ManageLinkedAccountsEffects, DynamicPropertyEffects,
-      DynamicEntityEffects, ProfileEffects,
-      OrganizationUnitEffects, MaintenanceEffects, DelegationsEffects, LoginEffects, LanguageTextEffects, DownloadEffects
+    EffectsModule.forRoot([
+      ProductEffects,
+      LoginAttemptsEffects,
+      MLAEffects,
+      LanguageEffects,
+      ManageLinkedAccountsEffects,
+      DynamicPropertyEffects,
+      DynamicEntityEffects,
+      ProfileEffects,
+      OrganizationUnitEffects,
+      MaintenanceEffects,
+      DelegationsEffects,
+      LoginEffects,
+      LanguageTextEffects,
+      DownloadEffects,
     ]),
     SharedModule,
     UserAuthModule,
     BrowserAnimationsModule,
-    RdsSideNavModule
-
+    RdsSideNavModule,
   ],
   declarations: [
     AppComponent,
@@ -126,9 +162,7 @@ const cookieConfig: RdsCookieConsentConfig = {
   providers: [
     DatePipe,
     { provide: API_BASE_URL, useFactory: getRemoteServiceBaseUrl },
-
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
