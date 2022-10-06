3b01d8a41f116661bb4d38d8ce155fe196e00eba
Alok Nakhate <alok.nakhate@waiin.com>
2022-10-06 16:58:16 +0530

    Resove mdx file rendering on storybook

--- raaghu-mfe/.storybook/preview.js
+++ raaghu-mfe/.storybook/preview.js
@@ -4,10 +4,10 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-  },
+  // options: {
+  //   storySort: (a, b) =>
+  //     a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  // },
   toolbar: {
     icon: 'circlehollow',
     // Array of plain string values or MenuItem shape (see below)
