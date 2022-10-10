7db14f4cfb52849693cce4679e2a97b162526944
Rohit Kulkarni <rohit.kulkarni@waiin.com>
2022-10-10 09:26:08 +0530

    Fixed UI bugs on storybook

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-background-image/background-image.stories.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-background-image/background-image.stories.ts
@@ -24,7 +24,7 @@ const Template: Story<RdsCompBackgroundImageComponent> = (args: RdsCompBackgroun
 export const Default = Template.bind({});
 
 Default.args = {
-    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLXV_IPvYFWoOnAfO3IGGxcCrM3Y2Bfgfciw&usqp=CAU',
+    imageUrl: 'https://cdn.pixabay.com/photo/2020/12/18/16/56/laptop-5842509_960_720.jpg',
     title: 'New arrivals are here',
     subtitle: 'The new arrivals have, well newly arrived. Check out the latest options from our summer small-batch release while they are still in stock.',
     btnLabel: 'CHECK NEW ARRIVALS HERE',
