a570a9fe677525b69c20d40dd59c94f56c232b33
Swamiraj Anarse <swamiraj.anarse@waiin.com>
2022-10-12 17:52:48 +0530

    committed-double-range-pc-checkbox-modal-counter-offcanvas

--- raaghu-elements/src/rds-double-range/src/lib/double-range.stories.ts
+++ raaghu-elements/src/rds-double-range/src/lib/double-range.stories.ts
@@ -6,15 +6,43 @@ import { RdsDoubleRangeComponent } from './rds-double-range.component';
 export default {
   title: 'Elements/Range',
   component: RdsDoubleRangeComponent,
-  argTypes: {
-
-  },
   decorators: [componentWrapperDecorator((story: any) => `<div style="margin: 3em">${story}</div>`)]
 } as Meta
 
 const Template: Story<RdsDoubleRangeComponent> = (args: RdsDoubleRangeComponent) => ({
-  props: args,
-
+  props: {...args},
+  template: `
+    <rds-double-range 
+      [role]="role" 
+      [val1]="val1" 
+      [val2]="val2" 
+      [min]="min" 
+      [max]="max">
+    </rds-double-range>`
 });
 export const DoubleRange = Template.bind({})
-DoubleRange.args = {}
+DoubleRange.args = {
+  role: 'Default',
+  val1: 0,
+  val2: 100,
+  min: 0,
+  max: 100
+}
+
+export const DoubleRangeType1 = Template.bind({})
+DoubleRangeType1.args = {
+  role: 'Double Range Type 1',
+  val1: 0,
+  val2: 100,
+  min: 0,
+  max: 100
+}
+
+export const DoubleRangeType2 = Template.bind({})
+DoubleRangeType2.args = {
+  role: 'Double Range Type 2',
+  val1: 0,
+  val2: 100,
+  min: 0,
+  max: 100
+}
