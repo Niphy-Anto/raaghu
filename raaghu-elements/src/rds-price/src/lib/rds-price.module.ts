56c79ae8cba0b38e0803eebcaf69d65d062040f8
Chinmay Wani <chinmay.wani@waiin.com>
2022-10-17 12:05:52 +0530

    committing the price module change

--- raaghu-elements/src/rds-price/src/lib/rds-price.module.ts
+++ raaghu-elements/src/rds-price/src/lib/rds-price.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RdsIconModule } from '@libs/rds-icon';
 import { RdsLabelModule } from '@libs/rds-label';
 import { RdsPriceComponent } from './rds-price.component';
