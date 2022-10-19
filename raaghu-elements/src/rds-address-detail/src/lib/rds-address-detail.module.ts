8832328b8ce591b49fbc5a1d0ade6fe4096f4f2e
Alok Nakhate <alok.nakhate@waiin.com>
2022-10-19 14:07:47 +0530

    Build issues

--- raaghu-elements/src/rds-address-detail/src/lib/rds-address-detail.module.ts
+++ raaghu-elements/src/rds-address-detail/src/lib/rds-address-detail.module.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RdsIconModule } from '@libs/rds-icon';
-import { RdsCardModule } from '../../../root/public-api';
 import { RdsAddressDetailComponent } from './rds-address-detail.component';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -12,7 +10,6 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   imports: [
     RdsIconModule,
-    RdsCardModule,
     CommonModule
   ],
   exports: [
--- raaghu-elements/src/rds-feed/src/lib/rds-feed.module.ts
+++ raaghu-elements/src/rds-feed/src/lib/rds-feed.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RdsFeedComponent } from './rds-feed.component';
-import { RdsAvatarModule } from 'rds-elements/rds-avatar/lib/rds-avatar.module';
-import { RdsLikeDislikeModule } from 'rds-elements/rds-like-dislike/lib/rds-like-dislike.module';
-import { RdsRatingModule } from 'rds-elements/rds-rating/lib/rds-rating.module';
-import { RdsIconModule } from 'rds-elements/rds-icon/lib/rds-icon.module';
-import { RdsLabelModule } from 'rds-elements/rds-label/lib/rds-label.module';
+import { RdsAvatarModule } from '@libs/rds-avatar';
+import { RdsLikeDislikeModule } from '@libs/rds-like-dislike';
+import { RdsRatingModule } from '@libs/rds-rating';
+import { RdsIconModule } from '@libs/rds-icon';
+import { RdsLabelModule } from '@libs/rds-label';
 
 @NgModule({
   declarations: [
