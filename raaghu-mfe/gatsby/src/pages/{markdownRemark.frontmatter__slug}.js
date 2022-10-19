25621eee7737047eac713e6445dc6fa238b2747f
Rahul Patil <55730676+rahulp34@users.noreply.github.com>
2022-10-19 17:10:37 +0530

    commited changes

--- raaghu-mfe/gatsby/src/pages/{markdownRemark.frontmatter__slug}.js
+++ raaghu-mfe/gatsby/src/pages/{markdownRemark.frontmatter__slug}.js
@@ -200,145 +200,152 @@ export default function Template({
   return (
     <div>
       <Navbar />
-      <div className="container-fluid mt-5 ">
-        <div className="row g-0">
-          <div className="col-md-2 ">
+      <div className="container-fluid mt-5 p-0 mx-0">
+        <div className="row">
+          <div id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse cust-scroll border-right">
             <Sidebar />
           </div>
-          <div className="col-md-8 m-lg-5 ">
-            <div className="blog-post-container">
-              <div className="blog-post ">
-                <div className="d-flex">
-                  <a href="/">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="img-fluid"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 32 32"
-                    >
-                      <g
-                        id="Component_128_1"
-                        data-name="Component 128 – 1"
-                        transform="translate(1 1)"
-                      >
-                        <line
-                          id="Line_88743"
-                          data-name="Line 88743"
-                          x2="11.826"
-                          transform="translate(9.088 15)"
-                          fill="none"
-                          stroke="#0900ff"
-                          // stroke-linecap="round"
-                          // stroke-linejoin="round"
-                          // stroke-width="2"
-                        />
-                        <path
-                          id="Path_129678"
-                          data-name="Path 129678"
-                          d="M18.054,24.287,13.127,19.36l4.927-4.927"
-                          transform="translate(-4.41 -4.36)"
-                          fill="none"
-                          stroke="#0900ff"
-                          // stroke-linecap="round"
-                          // stroke-linejoin="round"
-                          // stroke-width="2"
-                        />
-                        <circle
-                          id="Ellipse_8253"
-                          data-name="Ellipse 8253"
-                          cx="15"
-                          cy="15"
-                          r="15"
-                          fill="none"
-                          stroke="#0900ff"
-                          // stroke-linecap="round"
-                          // stroke-linejoin="round"
-                          // stroke-width="2"
-                        />
-                      </g>
-                    </svg>
-                  </a>
-                  <nav aria-label="breadcrumb" className="px-3">
-                    <ol className="breadcrumb">
-                      <li className="breadcrumb-item">
-                        <a href="/" className="link">
-                          {" "}
-                          {frontmatter.title.split(">")[0]}{" "}
+          <div className="col-md-9 ms-sm-auto col-lg-10 px-md-4 left-border">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="px-3 mt-4 pb-5 mb-5">
+                  <div className="blog-post-container">
+                    <div className="blog-post ">
+                      <div className="d-flex">
+                        <a href="/">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="img-fluid"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 32 32"
+                          >
+                            <g
+                              id="Component_128_1"
+                              data-name="Component 128 – 1"
+                              transform="translate(1 1)"
+                            >
+                              <line
+                                id="Line_88743"
+                                data-name="Line 88743"
+                                x2="11.826"
+                                transform="translate(9.088 15)"
+                                fill="none"
+                                stroke="#0900ff"
+                              // stroke-linecap="round"
+                              // stroke-linejoin="round"
+                              // stroke-width="2"
+                              />
+                              <path
+                                id="Path_129678"
+                                data-name="Path 129678"
+                                d="M18.054,24.287,13.127,19.36l4.927-4.927"
+                                transform="translate(-4.41 -4.36)"
+                                fill="none"
+                                stroke="#0900ff"
+                              // stroke-linecap="round"
+                              // stroke-linejoin="round"
+                              // stroke-width="2"
+                              />
+                              <circle
+                                id="Ellipse_8253"
+                                data-name="Ellipse 8253"
+                                cx="15"
+                                cy="15"
+                                r="15"
+                                fill="none"
+                                stroke="#0900ff"
+                              // stroke-linecap="round"
+                              // stroke-linejoin="round"
+                              // stroke-width="2"
+                              />
+                            </g>
+                          </svg>
                         </a>
-                      </li>
-                      <li
-                        className="breadcrumb-item active"
-                        aria-current="page"
-                      >
-                        {" "}
-                        {frontmatter.title.split(">")[1]}{" "}
-                      </li>
-                    </ol>
-                  </nav>
-                </div>
+                        <nav aria-label="breadcrumb" className="px-3">
+                          <ol className="breadcrumb">
+                            <li className="breadcrumb-item">
+                              <a href="/" className="link">
+                                {" "}
+                                {frontmatter.title.split(">")[0]}{" "}
+                              </a>
+                            </li>
+                            <li
+                              className="breadcrumb-item active"
+                              aria-current="page"
+                            >
+                              {" "}
+                              {frontmatter.title.split(">")[1]}{" "}
+                            </li>
+                          </ol>
+                        </nav>
+                      </div>
 
-                <div className="blog-post-content mt-4"
-                  dangerouslySetInnerHTML={{ __html: html }}>
+                      <div className="blog-post-content mt-4"
+                        dangerouslySetInnerHTML={{ __html: html }}>
 
+                      </div>
+                    </div>
                   </div>
+                </div>
               </div>
             </div>
             
           </div>
         </div>
-        </div>
         <div className="row">
           <div className="col-md-8 offset-2 justify-content-between bg-white">
-              <div>
-                <div className="bg-white bottom-0 d-flex justify-content-between page-navigation position-fixed py-4 px-4 footerbutton">
-                  <a onClick={backToPrev} className="text-decoration-none">
+            <div>
+              <div className="px-3 bg-white bottom-0 d-flex justify-content-between page-navigation position-fixed py-4 px-4 footerbutton">
+                <a onClick={backToPrev} className="text-decoration-none">
+                  <ul className="d-flex align-items-center">
+                    <li>
+                      <img src="images/prev-arrow.png" alt="next-arrow" className="" width="40px" />
+                    </li>
+                    <li>
+                      <nav className="pt-3 px-4">
+                        <div>
+                          <small className="text-uppercase" onClick={backToPrev}>Back to</small>
+                        </div>
+                        <ol className="breadcrumb">
+                          <li className="breadcrumb-item">
+                            {frontmatter.data}
+                          </li>
+                        </ol>
+                      </nav>
+                    </li>
+                  </ul>
+                </a>
+                <div>
+                  <a onClick={goToNext} className="text-decoration-none">
                     <ul className="d-flex align-items-center">
-                      <li>
-                        <img src="images/prev-arrow.png"  alt="next-arrow" className="" width="40px" />
-                      </li>
                       <li>
                         <nav className="pt-3 px-4">
                           <div>
-                            <small className="text-uppercase" onClick={backToPrev}>Back to</small>
+                            <small className="text-uppercase">
+                              Next
+                            </small>
                           </div>
-                          <ol className="breadcrumb">               
-                              <li className="breadcrumb-item">
+                          <ol className="breadcrumb">
+                            <li className="breadcrumb-item">
                               {frontmatter.data}
-                            </li> 
+
+                            </li>
                           </ol>
                         </nav>
                       </li>
+                      <li>
+                        <img src="images/next-arrow.png" alt="next-arrow" className="" width="40px" />
+                      </li>
                     </ul>
                   </a>
-                  <div>
-                    <a  onClick={goToNext} className="text-decoration-none">
-                      <ul className="d-flex align-items-center">
-                        <li>
-                          <nav className="pt-3 px-4">
-                            <div>
-                              <small className="text-uppercase">
-                                Next
-                              </small>
-                            </div>
-                            <ol className="breadcrumb">
-                              <li className="breadcrumb-item">
-                              {frontmatter.data}
-
-                              </li>
-                            </ol>
-                          </nav>
-                        </li>
-                        <li>
-                        <img src="images/next-arrow.png"  alt="next-arrow" className="" width="40px" />
-                        </li>
-                      </ul>
-                    </a>
-                  </div>
                 </div>
               </div>
             </div>
-            </div>
+          </div>
+        </div>
+        </div>
+        
       <Footer />
     </div>
   );
