f8c2cee09630cd34e023d7e09ed1558bf3cffa9b
Niphy-Anto <niphy.anto@waiin.com>
2022-10-28 09:55:06 +0530

    featurelist updated

--- raaghu-elements/src/rds-feature-list/src/lib/feature-list.stories.ts
+++ raaghu-elements/src/rds-feature-list/src/lib/feature-list.stories.ts
@@ -20,7 +20,7 @@ export default {
         display_type: {
             options: ['basic', 'advanced'],
             control: { type: 'select' }
-          },
+        },
     }
 } as Meta
 
@@ -29,7 +29,6 @@ const Template: Story<RdsFeatureListComponent> = (args: RdsFeatureListComponent)
 });
 
 export const Default = Template.bind({})
-Default.parameters = { controls: { include: ['header', 'listItems', 'columns'] } };
 Default.args = {
     header: 'Features',
     display_type: 'basic',
@@ -40,9 +39,7 @@ Default.args = {
                 {
                     id: 1, value: 'Only the best materials',
                     listItems: [
-                        {
-                            id: 1, value: 'Only the best materials',
-                        },
+                        { id: 1, value: 'Only the best materials' },
                         { id: 2, value: 'Ethically and locally made' },
                         { id: 3, value: 'Pre-washed and pre-shrunk' },
                         { id: 4, value: 'Machine wash cold with similar colors' },
@@ -50,27 +47,10 @@ Default.args = {
                         { id: 6, value: 'Double stitched construction' },
                         { id: 7, value: 'Water-resistant' },
                     ]
-                },
-                { id: 2, value: 'Ethically and locally made' },
-                { id: 3, value: 'Pre-washed and pre-shrunk' },
-                { id: 4, value: 'Machine wash cold with similar colors' },
-                { id: 5, value: 'Stainless strap loops' },
-                { id: 6, value: 'Double stitched construction' },
-                { id: 7, value: 'Water-resistant' },
+                }
             ],
         },
-        {
-            id: 2, value: 'Ethically and locally made',
-            listItems: [
-                { id: 1, value: 'Only the best materials', },
-                { id: 2, value: 'Ethically and locally made' },
-                { id: 3, value: 'Pre-washed and pre-shrunk' },
-                { id: 4, value: 'Machine wash cold with similar colors' },
-                { id: 5, value: 'Stainless strap loops' },
-                { id: 6, value: 'Double stitched construction' },
-                { id: 7, value: 'Water-resistant' },
-            ],
-        },
+        { id: 2, value: 'Ethically and locally made' },
         { id: 3, value: 'Pre-washed and pre-shrunk' },
         { id: 4, value: 'Machine wash cold with similar colors' },
         { id: 5, value: 'Stainless strap loops' },
