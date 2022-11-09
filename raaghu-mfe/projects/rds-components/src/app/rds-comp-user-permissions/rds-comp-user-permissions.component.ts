d788f468e9b3a020d3932a0df18bf8549024b72d
Pooja Kulkarni <107623732+poojakulkarniWai@users.noreply.github.com>
2022-11-09 15:03:02 +0530

    filter by permission Other than off canvas unable to see anything

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-user-permissions/rds-comp-user-permissions.component.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-user-permissions/rds-comp-user-permissions.component.ts
@@ -495,10 +495,18 @@ export class RdsCompUserPermissionsComponent implements OnInit {
       this.exportToExcel();
     }
     else if (event.key === 'filterByPermission') {
-      console.log(this.openFilterByPermission.nativeElement);
+      console.log("openFilterByPermission",this.openFilterByPermission.nativeElement);
       let el: HTMLElement = this.openFilterByPermission.nativeElement as HTMLElement;
       el.click();
     }
+  }
+  openCanvas(canvasId): void {
+    this.viewCanvas = true;
+    setTimeout(() => {
+      var offcanvas = document.getElementById(canvasId);
+      var bsOffcanvas = new bootstrap.Offcanvas(offcanvas);
+      bsOffcanvas.show();
+    }, 1);
   }
 
 }
