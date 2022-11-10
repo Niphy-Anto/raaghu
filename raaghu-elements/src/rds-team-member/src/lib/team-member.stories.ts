d0f0ee1326d98a244e402127dddc727020beb6de
suraj-khupse <99809670+suraj-khupse@users.noreply.github.com>
2022-11-10 09:59:50 +0530

    resloved stroies files issue.

--- raaghu-elements/src/rds-team-member/src/lib/team-member.stories.ts
+++ raaghu-elements/src/rds-team-member/src/lib/team-member.stories.ts
@@ -1,13 +1,11 @@
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RdsIconModule } from '@libs/rds-icon';
-import { RdsProductImageModule } from '@libs/rds-product-image';
-
-import { RdsLabelModule } from '@libs/rds-label';
 import { RdsAvatarModule } from '@libs/rds-avatar';
+import { RdsIconModule } from '@libs/rds-icon';
+import { RdsLabelModule } from '@libs/rds-label';
+import { RdsProductImageModule } from '@libs/rds-product-image';
 import { RdsTeamMemberComponent } from '@libs/rds-team-member';
-// import { RdsProductImageModule } from '@libs/rds-product-image';
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
 
 export default {
   title: 'Elements/Team-member',
@@ -15,40 +13,58 @@ export default {
   decorators: [
     moduleMetadata({
       declarations: [],
-      imports: [CommonModule, FormsModule, RdsIconModule, RdsProductImageModule, RdsLabelModule, RdsAvatarModule
+      imports: [
+        CommonModule,
+        FormsModule,
+        RdsIconModule,
+        RdsProductImageModule,
+        RdsLabelModule,
+        RdsAvatarModule
       ],
     }),
   ],
-  argTypes: {
-    displayType: { table: { disable: true, }, },
-  }
- } as Meta;
+  // argTypes: {
+  //   displayType: { table: { disable: true, }, },
+  // },
+} as Meta;
 
-  const Template: Story<RdsTeamMemberComponent> = (args: RdsTeamMemberComponent) => ({
-    props: args,
-  });
+const Template: Story<RdsTeamMemberComponent> = (args: RdsTeamMemberComponent) => ({
+  props: args
+});
 
 export const Default = Template.bind({});
-Default.parameters = { controls: { include: ['displayType', 'teamItem'] } };
+// Default.parameters = { controls: { include: ['displayType', 'teamItem'] } };
 Default.args = {
-  displayType: 'basic',
-  teamItem: [
+  displayType : "Basic",
+  colorVariant: "primary",
+  teamItem : [{
+      title: 'Tina',
+      subTitle: 'Web Developer',
+      imgLink: 'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
+      description: 'Lorem ipsum dolor sit amet conr adipiscing elit',
+  }]
+};
+
+
+export const advance = Template.bind({});
+advance.args = {
+  displayType: 'Advanced',
+  colorVariant: 'primary',
+  teamItem : [
     {
       title: 'Tina',
       subTitle: 'Web Developer',
       imgLink: 'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
-      description: 'Lorem ipsum dolor sit amet conr adipiscing elit'
-    }
-  ]
+      description: 'Lorem ipsum dolor sit amet conr adipiscing elit',
 
-}
+    },
+    {
+      title: 'Tina',
+      subTitle: 'Web Developer',
+      imgLink: 'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png?W=100',
+      description: 'Lorem ipsum dolor sit amet conr adipiscing elit',
 
-export const advance = Template.bind({});
-advance.parameters = { controls: { include: ['displayType', 'teamItem'] } };
-advance.args = {
-  displayType: 'advanced',
-  colorVariant: 'primary',
-  teamItem: [
+    },
     {
       title: 'Tina',
       subTitle: 'Web Developer',
@@ -56,6 +72,8 @@ advance.args = {
       description: 'Lorem ipsum dolor sit amet conr adipiscing elit',
 
     }
+
   ]
 
 }
+
