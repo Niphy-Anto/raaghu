44a4c3392d7e83e7a3eaf0627fbe2789d65ae6b6
Rahul Patil <55730676+rahulp34@users.noreply.github.com>
2022-11-21 10:11:11 +0530

    commited changes

--- raaghu-elements/src/rds-chart-bar-horizontal/src/lib/rds-chart-bar-horizontal.component.ts
+++ raaghu-elements/src/rds-chart-bar-horizontal/src/lib/rds-chart-bar-horizontal.component.ts
@@ -23,7 +23,7 @@ export class RdsChartBarHorizontalComponent implements OnInit {
   canvas: any;
   ctx: any;
   //chartId = 'horizontalChart' + RdsChartBarHorizontalComponent.count;
-  @Input() chartId:string='horizontalChart0';
+  @Input() chartId: string = 'horizontalChart0';
   @Input() chartWidth = 100;
   @Input() chartHeight = 350;
   @Input() chartLabels?: any
@@ -35,16 +35,7 @@ export class RdsChartBarHorizontalComponent implements OnInit {
 
   ngOnInit(): void {
     this.style = getComputedStyle(document.body);
-/*     this.chartDataSets[0].backgroundColor[0] = this.style.getPropertyValue('--chartColor1');
-    this.chartDataSets[0].backgroundColor[1] = this.style.getPropertyValue('--chartColor2');
-    this.chartDataSets[0].backgroundColor[2] = this.style.getPropertyValue('--chartColor3');
-    this.chartDataSets[0].backgroundColor[3] = this.style.getPropertyValue('--chartColor4');
-    this.chartDataSets[0].backgroundColor[4] = this.style.getPropertyValue('--chartColor5');
-    this.chartDataSets[0].backgroundColor[5] = this.style.getPropertyValue('--chartColor6');
-    this.chartDataSets[0].backgroundColor[6] = this.style.getPropertyValue('--chartColor7');
-    this.chartDataSets[0].backgroundColor[7] = this.style.getPropertyValue('--chartColor8');
-    this.chartDataSets[0].backgroundColor[8] = this.style.getPropertyValue('--chartColor9');
-    this.chartDataSets[0].backgroundColor[9] = this.style.getPropertyValue('--chartColor10'); */
+
   }
 
   ngOnChanges(): void {
@@ -64,6 +55,13 @@ export class RdsChartBarHorizontalComponent implements OnInit {
     }
     this.canvas = document.getElementById(this.chartId);
     if (this.canvas !== null) {
+      this.chartDataSets.forEach((element: any) => {
+        element.backgroundColor.forEach((bg: any, index: number) => {
+          if (this.style) {
+            element.backgroundColor[index] = (this.style.getPropertyValue('--chart-bar-horizontal-color' + index + 1)) ? this.style.getPropertyValue('--chart-bar-horizontal-color' + index + 1) : bg
+          }
+        });
+      });
       this.ctx = this.canvas.getContext('2d');
       const canvas = new Chart(this.ctx, {
         type: 'bar',
@@ -73,10 +71,10 @@ export class RdsChartBarHorizontalComponent implements OnInit {
         },
         options: this.chartOptions
       });
-      if(canvas !== null){
-        canvas.canvas.style.height = this.chartHeight+'px'; 
-        canvas.canvas.style.width = this.chartWidth+'px';
-      }    
+      if (canvas !== null) {
+        canvas.canvas.style.height = this.chartHeight + 'px';
+        canvas.canvas.style.width = this.chartWidth + 'px';
+      }
     }
   }
 
