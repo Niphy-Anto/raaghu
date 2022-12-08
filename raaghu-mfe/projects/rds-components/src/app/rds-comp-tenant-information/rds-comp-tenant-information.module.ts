bda8a8ebd2ff846eb5a4eb665aa84d4142b5a38c
Bhorga <harshit.bhorga@waiin.com>
2022-12-08 17:15:43 +0530

    tenant list updated

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-tenant-information/rds-comp-tenant-information.module.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-tenant-information/rds-comp-tenant-information.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RdsCompTenantInformationComponent } from './rds-comp-tenant-information.component';
 import { FormsModule } from '@angular/forms';
-import { RdsCheckboxModule, RdsDatepickerModule, RdsDropdownlistModule, RdsInputModule } from '@libs/rds-elements';
+import { RdsButtonModule, RdsCheckboxModule, RdsDatepickerModule, RdsDropdownlistModule, RdsInputModule } from '@libs/rds-elements';
 import { RdsCompTenantInformationShimmerComponent } from './rds-comp-tenant-information-shimmer/rds-comp-tenant-information-shimmer.component';
+import { NgxTranslateModule } from '@libs/shared';
 
 
 
@@ -14,10 +15,11 @@ import { RdsCompTenantInformationShimmerComponent } from './rds-comp-tenant-info
     CommonModule,
     FormsModule,
     RdsInputModule,
+    RdsButtonModule,
     RdsDropdownlistModule,
     RdsCheckboxModule,
     RdsDatepickerModule,
-
+    NgxTranslateModule.forRoot()
   ]
 })
 export class RdsCompTenantInformationModule { }
