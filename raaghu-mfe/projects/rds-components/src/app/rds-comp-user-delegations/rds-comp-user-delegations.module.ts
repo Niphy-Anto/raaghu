769a2f94fdbcbf16306d91194e3d3901075aaf40
bhorga <harshit.bhorga@waiin.com>
2022-12-08 14:26:42 +0530

    userdelegation module

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-user-delegations/rds-comp-user-delegations.module.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-user-delegations/rds-comp-user-delegations.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RdsUserDelegationsComponent } from './rds-comp-user-delegations.component';
-import { RdsButtonModule, RdsDatepickerModule, RdsDropdownlistModule } from '@libs/rds-elements';
+import { RdsButtonModule, RdsDatepickerModule, RdsDropdownlistModule, RdsLabelModule } from '@libs/rds-elements';
 import { RdsCompDataTableModule } from '../rds-comp-data-table/rds-comp-data-table.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +16,8 @@ import { RdsCompDataTableModule } from '../rds-comp-data-table/rds-comp-data-tab
     RdsDropdownlistModule,
     RdsDatepickerModule,
     RdsCompDataTableModule,
-    
+    RdsLabelModule,
+    FormsModule
   ]
 })
 export class RdsCompUserDelegationsModule { }
