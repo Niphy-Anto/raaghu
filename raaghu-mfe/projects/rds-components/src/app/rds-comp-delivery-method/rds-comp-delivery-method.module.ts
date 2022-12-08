51f0504b70bc4076bc77fe0cbb992602b023be14
Alok Nakhate <alok.nakhate@waiin.com>
2022-12-08 19:06:24 +0530

    Components are converted to individual Module

--- raaghu-mfe/projects/rds-components/src/app/rds-comp-delivery-method/rds-comp-delivery-method.module.ts
+++ raaghu-mfe/projects/rds-components/src/app/rds-comp-delivery-method/rds-comp-delivery-method.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RdsCompDeliveryMethodComponent } from './rds-comp-delivery-method.component';
+import { RdsButtonModule } from "@libs/rds-elements";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RdsCompDeliveryMethodComponent
+  ],
+  exports: [
+    RdsCompDeliveryMethodComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RdsButtonModule
   ]
 })
 export class RdsCompDeliveryMethodModule { }
